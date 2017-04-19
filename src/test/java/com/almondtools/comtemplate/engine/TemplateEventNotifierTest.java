2aed2318ad0a930d9bc84c44b413781130e19d01
almondtools <mandelst@gmail.com>
2017-04-19 11:23:10 +0200

    update Mockito

--- src/test/java/com/almondtools/comtemplate/engine/TemplateEventNotifierTest.java
+++ src/test/java/com/almondtools/comtemplate/engine/TemplateEventNotifierTest.java
@@ -14,7 +14,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import com.almondtools.comtemplate.engine.expressions.Cast;
 import com.almondtools.comtemplate.engine.expressions.Concat;
